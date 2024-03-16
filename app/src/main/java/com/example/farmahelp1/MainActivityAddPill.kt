023589cf94934ba86720cfb6474dc170d572398f
Андрей Арустамян <>
2024-03-16 21:21:46 +0300

    no message

--- app/src/main/java/com/example/farmahelp1/MainActivityAddPill.kt
+++ app/src/main/java/com/example/farmahelp1/MainActivityAddPill.kt
@@ -6,12 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity2 : AppCompatActivity() {
+class MainActivityAddPill : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_add_pills)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_add_pills)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
