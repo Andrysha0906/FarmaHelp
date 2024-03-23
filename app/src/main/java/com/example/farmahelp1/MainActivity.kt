adf24cfaab300bb61fdde5301e2c4ccb6206967f
Андрей Арустамян <>
2024-03-23 15:30:36 +0300

    .

--- app/src/main/java/com/example/farmahelp1/MainActivity.kt
+++ app/src/main/java/com/example/farmahelp1/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.farmahelp1
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
--- app/src/main/java/com/example/farmahelp1/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/farmahelp1/ui/home/HomeFragment.kt
@@ -1,11 +1,18 @@
 package com.example.farmahelp1.ui.home
 
+import android.adservices.adid.AdId
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.farmahelp1.MainActivity
+import com.example.farmahelp1.MainActivitySearch
+import com.example.farmahelp1.R
 import com.example.farmahelp1.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -22,7 +29,6 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
         return root
